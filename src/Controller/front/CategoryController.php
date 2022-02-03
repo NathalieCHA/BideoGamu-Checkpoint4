36ad8f54a95d6150e3a878db0b7475f0e2910175
CHARLES Nathalie <charlnathalie@gmail.com>
2022-02-03 21:53:30 +0100

    the end

--- src/Controller/front/CategoryController.php
+++ src/Controller/front/CategoryController.php
@@ -3,13 +3,16 @@
 namespace App\Controller\front;
 
 use App\Entity\Category;
+use App\Entity\Videogames;
+use App\Repository\CategoryRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+#[Route('/category')]
 class CategoryController extends AbstractController
 {
-    #[Route('/category', name: 'category')]
+    #[Route('/', name: 'category')]
     public function index(): Response
     {
         $categories = $this->getDoctrine()
@@ -19,4 +22,35 @@ class CategoryController extends AbstractController
         return $this->render('front/category/index.html.twig',
         ['categories' => $categories]);
     }
+
+    #[Route('/{name}', name: 'category_show', methods: ['GET'])]
+    public function show(string $name): Response
+    {
+        $category = $this->getDoctrine()
+            ->getRepository(Category::class)
+            ->findBy(
+                ['name' => $name],
+            );
+
+        if (empty($category)) {
+            throw $this->createNotFoundException(
+                'No category with id : ' . $name . ' found.'
+            );
+        }
+
+        $categoryId = $category[0]->getId();
+
+        $videogames = $this->getDoctrine()
+            ->getRepository(Videogames::class)
+            ->findBy(
+                ['category' => $categoryId],
+                ['id' => 'DESC'],
+                3
+            );
+
+        return $this->render('front/category/show.html.twig', [
+            'name' => $name, 'videogames' => $videogames
+        ]);
+    }
+
 }
